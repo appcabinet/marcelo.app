aba98be64558cd1f10474c41d01dac69a2647d25
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-04-04 06:40:30 -0400

    chore: Enable SSR mode in astro.config for example.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  output: 'server',  // Enable SSR mode
   integrations: [mdx(), sitemap()],
   adapter: vercel(),
 });
