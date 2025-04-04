82fb17bc612a973fda9e650729b2ed8f2adc13d7
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-04-04 09:46:09 -0400

    chore: Refactor file names for consistency and update footer component names.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Marcelo Mantilla';
-export const SITE_DESCRIPTION = 'Product engineer and creative writer based out of Toronto / Houston';
+export const SITE_DESCRIPTION = 'Notes, articles, and essays by Marcelo Mantilla';
