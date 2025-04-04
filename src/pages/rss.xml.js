3294e662417257bdb8a94f4b57d66ad49f921337
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-04-04 12:55:51 -0400

    chore: Update link structure in rss.xml.js and style tweaks in NoteList.astro

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: notes.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/${post.id}/`,
 		})),
 	});
 }
