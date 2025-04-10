93d3bac80b00544bc07315d523d899458923c4b9
Marcelo Mantilla <marcelo.mantilla@appcabinet.com>
2025-04-10 10:06:19 -0400

    chore: Adjust margins and font sizes for consistency in styles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Marcelo Mantilla';
+export const SITE_TITLE = `Marcelo Mantilla${import.meta.env.DEV ? ' (Localhost)' : ''}`;
 export const SITE_DESCRIPTION = 'Notes, articles, and essays by Marcelo Mantilla';
